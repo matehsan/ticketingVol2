34546b494598cfa455a9d331c4b5e6c6613c78c2
ehsangun <matari.ehsan@yahoo.com>
2019-07-16 05:52:12 +0430

    product & ticket crud with gii generated

---
+++ backend/views/ticket/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Ticket */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ticket-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'subject')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'message')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'customer_id')->textInput() ?>
+
+    <?= $form->field($model, 'admin_id')->textInput() ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <?= $form->field($model, 'is_answered')->textInput() ?>
+
+    <?= $form->field($model, 'is_closed')->textInput() ?>
+
+    <?= $form->field($model, 'product_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
